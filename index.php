b6ba15851fc90b7d81c399ba972b420bfaa28b32
Tom Rockwell <devospice@gmail.com>
2016-10-26 10:31:14 -0400

    desktop home page complete

--- index.php
+++ index.php
@@ -61,6 +61,16 @@
 			</div>
 		
 		</div>
+		
+		<footer>
+			<ul>
+				<li><a href="#">Contact Us</a></li>
+				<li>|</li>
+				<li><a href="#">Terms of Use</a></li>
+				<li>|</li>
+				<li><a href="#">About Sun Pharma</a></li>
+			</ul>
+		</footer>
 	
 	</div>
 
